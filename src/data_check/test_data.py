df6c9d1ba871e3c03873532e5558abee2e87da61
wojciech.szenic <wojciech.szenic@olx.pl>
2022-11-11 11:22:49 +0100

    WIP project with fixed dependencies

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -60,6 +60,19 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
+def test_row_count(data):
+    """
+    Test that the number of rows is the same as the reference dataset
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
+    """
+    Test that the price range is the same as the reference dataset
+    """
+    assert data['price'].between(min_price, max_price).all()
+
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
